630b0a1f7c1039408c2864bf71504ae2dbfb893e
Yaroslav <y.suvorov@yadro.com>
2022-03-07 23:12:54 +0300

    add main logic

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import requests
+from dotenv import load_dotenv
+from vk_api import publish_comic
+from xkcd import get_comic_to_post
+from file_operations import download_picture
+
+
+def main():
+    load_dotenv()
+    try:
+        comic = get_comic_to_post()
+        download_picture(comic['comic_link'], comic['comic_filename'])
+        publish_comic(comic)
+    except requests.exceptions.HTTPError:
+        print('Не удалось установить соединение с сервером')
+    except IOError:
+        print('Не получилось скачать файл')
+    finally:
+        os.remove(comic['comic_filename'])
+
+
+if __name__=='__main__':
+    main()
